d3a3ed2a63ecff8ae0ca7bf96a3e4fdfe0e353dd
lofitemi9 <temiloluwashokubi@gmail.com>
2026-03-22 14:58:34 -0400

    Add MusicHistory model and new DB connection string
    Added MusicHistory class to represent user listening history, including fields for song, artist, album, and user info. Updated appsettings.json with a new MusicHistoryConnection string pointing to the master database on the SQL Server.

---
+++ Models/MuiscHistory.cs
@@ -0,0 +1,14 @@
+﻿namespace SpotifyStatisticsWebApp.Models
+{
+    public class MusicHistory
+    {
+        public DateTime PlayedAt { get; set; }
+        public string? Song { get; set; }
+        public string? Artist { get; set; }
+        public string? Album { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Country { get; set; }
+        public string? BeginArea { get; set; }
+        public string? UserId { get; set; }
+    }
+}
