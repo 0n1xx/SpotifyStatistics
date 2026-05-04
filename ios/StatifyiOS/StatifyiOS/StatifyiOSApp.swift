e0039fe3e66098a5106d2d06f91aee8cb4e335ae
Vladislav <vladsahar27@gmail.com>
2026-05-03 23:57:50 -0400

    Fixes

---
+++ ios/StatifyiOS/StatifyiOS/StatifyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatifyiOSApp.swift
+//  StatifyiOS
+//
+//  Created by Vlad Sakharov on 2026-05-03.
+//
+
+import SwiftUI
+
+@main
+struct StatifyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
