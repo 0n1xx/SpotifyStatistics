6e4955c4740d4c4a3a86d9654c4b9503bd391809
Vladislav <vladsahar27@gmail.com>
2026-06-24 22:50:36 -0400

    Small fixes

--- web/Services/SqlConnectionFactory.cs
+++ web/Services/SqlConnectionFactory.cs
@@ -14,6 +14,7 @@ public static class SqlConnectionFactory
         {
             Encrypt = true,
             TrustServerCertificate = true,
+            ConnectTimeout = 60,
         }.ConnectionString;
     }
 }
